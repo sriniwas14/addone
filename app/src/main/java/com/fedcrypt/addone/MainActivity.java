14c91bbce01fbeaaade646989b299ec6317a63c8
sriniwas <sriniwasx@gmail.com>
2019-11-10 13:24:29 +0530

    Added Splash screen and HomeActivity

--- app/src/main/java/com/fedcrypt/addone/MainActivity.java
+++ app/src/main/java/com/fedcrypt/addone/MainActivity.java
@@ -2,7 +2,12 @@ package com.fedcrypt.addone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        Timer t = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        };
+
+        t.schedule(task, 200);
     }
 }
